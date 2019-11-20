bf069ebe63d38a64d36b89780f71c2fc946b4b0b
Arne Kvaleberg <arnekva@gmail.com>
2019-11-20 16:26:17 +0100

    I guess we have to fix stuff

--- src/no/hvl/dat152/obl4/database/SearchItemDAO.java
+++ src/no/hvl/dat152/obl4/database/SearchItemDAO.java
@@ -1,6 +1,7 @@
 package no.hvl.dat152.obl4.database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -10,75 +11,76 @@ import no.hvl.dat152.obl4.util.Validator;
 
 public class SearchItemDAO {
 
-  public List<SearchItem> getSearchHistoryLastFive() {
-    String sql = "SELECT * FROM SecOblig.History ORDER BY datetime DESC";
-    // LIMIT 5
-    // Derby lacks LIMIT
-    return getSearchItemList(sql,5);
-  }
+	public List<SearchItem> getSearchHistoryLastFive() {
+		String sql = "SELECT * FROM SecOblig.History ORDER BY datetime DESC";
+		// LIMIT 5
+		// Derby lacks LIMIT
+		return getSearchItemList(sql, 5);
+	}
 
-  public List<SearchItem> getSearchHistoryForUser(String username) {
-    String sql = "SELECT * FROM SecOblig.History " 
-        + "WHERE username = '" + Validator.validString(username) 
-        + "' ORDER BY datetime DESC";
-    //  LIMIT 50
-    // Derby lacks LIMIT
-    return getSearchItemList(sql,50);
-  }
+	public List<SearchItem> getSearchHistoryForUser(String username) {
+		String sql = "SELECT * FROM SecOblig.History WHERE username = ? ORDER BY datetime DESC";
+		//String query 
+		// LIMIT 50
+		// Derby lacks LIMIT
+		return getSearchItemList(sql, 50);
+	}
 
-  private List<SearchItem> getSearchItemList(String sql,Integer limit) {
+	private List<SearchItem> getSearchItemList(String sql, Integer limit) {
 
-    List<SearchItem> result = new ArrayList<SearchItem>();
+		List<SearchItem> result = new ArrayList<SearchItem>();
 
-    Connection c = null;
-    Statement s = null;
-    ResultSet r = null;
+		Connection c = null;
+		Statement s = null;
+		ResultSet r = null;
 
-    try {        
-      c = DatabaseHelper.getConnection();
-      s = c.createStatement();
-      if (limit > 0) s.setMaxRows(limit);
-      r = s.executeQuery(sql);
+		try {
+			c = DatabaseHelper.getConnection();
+			s = c.createStatement();
+			if (limit > 0)
+				s.setMaxRows(limit);
+			r = s.executeQuery(sql);
 
-      while (r.next()) {
-        SearchItem item = new SearchItem(
-            r.getTimestamp("datetime"),
-            r.getString("username"),
-            r.getString("searchkey")
-            );
-        result.add(item);
-      }
+			while (r.next()) {
+				SearchItem item = new SearchItem(r.getTimestamp("datetime"), r.getString("username"),
+						r.getString("searchkey"));
+				result.add(item);
+			}
 
-    } catch (Exception e) {
-      System.out.println(e);
-    } finally {
-      DatabaseHelper.closeConnection(r, s, c);
-    }
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			DatabaseHelper.closeConnection(r, s, c);
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  public void saveSearch(SearchItem search) {
+	public void saveSearch(SearchItem search) {
 
-    String sql = "INSERT INTO SecOblig.History VALUES (" 
-        + "'" + search.getDatetime()  + "', "
-        + "'" + search.getUsername()  + "', "
-        + "'" + search.getSearchkey() + "')";
+		String sql = "INSERT INTO SecOblig.History VALUES (" + "'" + search.getDatetime() + "', " + "'"
+				+ search.getUsername() + "', " + "'" + search.getSearchkey() + "')";
+		String query = "INSERT INTO SecOblig.History VALUES (?, ?, ?)";
 
-    Connection c = null;
-    Statement s = null;
-    ResultSet r = null;
+		Connection c = null;
+		ResultSet r = null;
+		PreparedStatement pstmt = null;
 
-    try {        
-      c = DatabaseHelper.getConnection();
-      s = c.createStatement();       
-      s.executeUpdate(sql);
+		try {
 
-    } catch (Exception e) {
-      System.out.println(e);
-    } finally {
-      DatabaseHelper.closeConnection(r, s, c);
-    }
-  }
+			c = DatabaseHelper.getConnection();
+			pstmt = c.prepareStatement(query);
+			pstmt.setString(1, search.getDatetime().toString());
+			pstmt.setString(2, search.getUsername());
+			pstmt.setString(3, search.getSearchkey());
+
+			pstmt.execute();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			DatabaseHelper.closeConnection(r, pstmt, c);
+		}
+	}
 
 }
